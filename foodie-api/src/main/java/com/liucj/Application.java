56f70c04994f0d284291968cf5e1ae3e6bb488a6
lchj <627107345@qq.com>
2020-06-05 17:13:00 +0800

    集成MyBatis

---
+++ foodie-api/src/main/java/com/liucj/Application.java
@@ -0,0 +1,12 @@
+package com.liucj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    //psvm
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
