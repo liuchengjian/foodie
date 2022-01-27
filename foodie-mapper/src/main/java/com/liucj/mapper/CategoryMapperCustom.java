1dad127f4e99e807b91f7a0ea39a74d827c72091
liucj <627107345@qq.com>
2022-01-27 10:39:24 +0800

    修改不能获得自定义mapper的bug

--- foodie-mapper/src/main/java/com/liucj/mapper/CategoryMapperCustom.java
+++ foodie-mapper/src/main/java/com/liucj/mapper/CategoryMapperCustom.java
@@ -2,11 +2,14 @@ package com.liucj.mapper;
 
 import com.liucj.pojo.vo.CategoryVO;
 import com.liucj.pojo.vo.NewItemsVO;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
-
+@Component
+@Mapper
 public interface CategoryMapperCustom {
 
     public List<CategoryVO> getSubCatList(Integer rootCatId);
